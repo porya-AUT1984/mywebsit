e37a003b27ad1b5180eb5a8e7ff7dc1a677f1d0d
porya zarie <porya.aut1984@gmail.com>
2020-04-13 18:28:12 +0430

    about

--- wp-content/themes/Project/home-page.php
+++ wp-content/themes/Project/home-page.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Template Name: Home Page
 */
@@ -9,79 +9,79 @@ Template Name: Home Page
 
 
 <div class="secSpe">
-    
+
    <i class="ion-gear-b"></i> <p>My <span>Specialties</span> </p>
-    
+
 </div>
 
 
 <section class="special">
-   
-    
+
+
     <div class="row box1">
-        
+
         <div class="col span-1-of-3">
-           
+
         <i class="ion-social-wordpress"></i>
         <h2><?php the_field("heading_one"); ?></h2>
-        <p><?php the_field("description_one"); ?></p>   
-            
-            
+        <p><?php the_field("description_one"); ?></p>
+
+
         </div>
-        
-        
+
+
         <div class="col span-1-of-3">
          <i class="ion-iphone"></i>
         <h2><?php the_field("heading_two"); ?></h2>
-        <p><?php the_field("description_two"); ?></p>      
-            
+        <p><?php the_field("description_two"); ?></p>
+
         </div>
-        
-        
+
+
         <div class="col span-1-of-3">
         <i class="ion-chatbubbles"></i>
         <h2><?php the_field("heading_three"); ?></h2>
-        <p><?php the_field("description_three"); ?></p>     
-            
+        <p><?php the_field("description_three"); ?></p>
+
         </div>
-        
-        
+
+
     </div> <!--Row Ends Here-->
-    
-    
-    
-    
+
+
+
+
     <div class="row box1">
-        
+
         <div class="col span-1-of-3">
-           
+
         <i class="ion-email-unread"></i>
         <h2><?php the_field("heading_four"); ?></h2>
-        <p><?php the_field("description_four"); ?></p>   
-            
-            
+        <p><?php the_field("description_four"); ?></p>
+
+
         </div>
-        
-        
+
+
         <div class="col span-1-of-3">
          <i class="ion-ios-search-strong"></i>
         <h2><?php the_field("heading_five"); ?></h2>
-        <p><?php the_field("description_five"); ?></p>      
-            
+        <p><?php the_field("description_five"); ?></p>
+
         </div>
-        
-        
+
+
         <div class="col span-1-of-3">
         <i class="ion-android-contacts"></i>
         <h2><?php the_field("heading_six"); ?></h2>
-        <p><?php the_field("description_six"); ?></p>     
-            
+        <p><?php the_field("description_six"); ?></p>
+
         </div>
-        
-        
+
+
     </div> <!--Row Ends Here-->
-    
-    
+
+
 </section> <!--Section My Specialties Ends Here-->
 
 
@@ -90,36 +90,36 @@ Template Name: Home Page
 
 
 <div class="secSpe">
-    
+
    <i class="ion-cube"></i> <p>My <span>Packages</span> </p>
-    
+
 </div>
 
 <p class="packageP"><?php the_field('mypackages_title'); ?></p>
 
 <section class="packages">
-   
-   
+
+
    <div class="row">
-      
-      
+
+
       <div class="col span-1-of-3 box2">
          <p><?php the_field('box_one_price'); ?></p>
          <h3><?php the_field('box_one_title'); ?></h3>
-         
+
          <ul>
-           
+
            <?php the_field('box_one_description'); ?>
-            
+
             <li><a href="http://localhost/wordpress/contact/">inquery</a></li>
 
-    
+
         </ul>
-          
+
       </div>
-      
-      
-      
+
+
+
             <div class="col span-1-of-3 box3">
          <p> <?php the_field('box_two_price'); ?></p>
          <h3><?php the_field('box_two_title'); ?></h3>
@@ -127,13 +127,13 @@ Template Name: Home Page
             <?php the_field('box_two_description'); ?>
             <li><a href="http://localhost/wordpress/contact/">inquery</a></li>
 
-    
+
         </ul>
-          
+
       </div>
-      
-      
-      
+
+
+
             <div class="col span-1-of-3 box4">
          <p><?php the_field('box_three_price'); ?></p>
          <h3><?php the_field('box_three_title'); ?></h3>
@@ -142,38 +142,38 @@ Template Name: Home Page
 
             <li><a href="http://localhost/wordpress/contact/">inquery</a></li>
 
-    
+
         </ul>
-          
+
       </div>
-       
-       
-       
+
+
+
    </div>
-    
-    
-    
-    
+
+
+
+
 </section>
 <!--My Packages Section Ends here -->
- 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
 <!--My Portfolio section starts here-->
 
 
 <div class="secSpe porTitle">
-    
+
    <i class="ion-briefcase"></i> <p>My <span>Portfolio</span> </p>
-    
+
 </div>
 
 <section class="secPort">
-  
+
   <?php $loop = new WP_query(array(
 
     'post_type' => 'portfolio',
@@ -185,27 +185,27 @@ Template Name: Home Page
 
 
 )); ?>
-   
+
    <?php if($loop->have_posts() ) : ?>
    <?php while ($loop->have_posts() ) : $loop->the_post(); ?>
-   
 
-   
-      
+
+
+
    <div class="row portRow1">
-      
-      
+
+
       <div class="col span-1-of-4">
-       
+
        <?php $image = get_field('image_one'); ?>
        <?php if(!empty($image)) : ?>
         <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
         <?php endif; ?>
-        
+
         <p><?php the_field('title_one'); ?></p>
         <?php the_field('link_one'); ?>
       </div>
-      
+
           <div class="col span-1-of-4">
          <?php $image = get_field('image_two'); ?>
        <?php if(!empty($image)) : ?>
@@ -214,7 +214,7 @@ Template Name: Home Page
              <p><?php the_field('title_two'); ?></p>
         <?php the_field('link_two'); ?>
       </div>
-      
+
           <div class="col span-1-of-4">
        <?php $image = get_field('image_three'); ?>
        <?php if(!empty($image)) : ?>
@@ -223,7 +223,7 @@ Template Name: Home Page
               <p><?php the_field('title_three'); ?></p>
         <?php the_field('link_three'); ?>
       </div>
-      
+
           <div class="col span-1-of-4">
         <?php $image = get_field('image_four'); ?>
        <?php if(!empty($image)) : ?>
@@ -232,15 +232,15 @@ Template Name: Home Page
              <p><?php the_field('title_four_'); ?></p>
         <?php the_field('link_four'); ?>
       </div>
-       
+
 
    </div> <!--Row Ends Here-->
-   
-    
+
+
       <?php endwhile; ?>
    <?php endif; ?>
-   
-   
+
+
 </section>
 
 <!--My Portfolio section Ends here-->
@@ -254,33 +254,36 @@ Template Name: Home Page
 
 
 <div class="secSpe aboutTitle">
-    
+
    <i class="ion-person-stalker"></i> <p>About <span>Me</span> </p>
-    
+
 </div>
 
 <section class="aboutSec">
-   
-   
+
+
    <div class="row aboutBox">
-       
+
        <div class="col span-3-of-3">
-         
-         <?php $image1 = get_field('my_image', 21); ?>
+
+         <?php $image1 = get_field('my_image', 9); ?>
          <?php if(!empty($image1)) : ?>
-         <img src="<?php echo $image1['url']; ?>" alt="<?php echo $image1['alt']; ?>">
+         <img src="<?php echo $image1['url']; ?>" alt="<?php echo $image1['alt']; ?>"
+      onmouseover=""   >
          <?php endif; ?>
-          
-          
-          <p class="aboutP1"><?php the_field('paragraph_one', 21); ?> </p>
-          
-          <p class="aboutP2"><?php the_field('paragraph_two', 21); ?></p>
-           
+
+
+          <p class="aboutP1"><?php the_field('first_paragraph', 9); ?> </p>
+
+          <p class="aboutP2"><?php the_field('second_paragraph', 9); ?></p>
+
+          <a  class="aboutmail" href="<?php the_field('email', 9); ?>" > email </a>
+
        </div>
-       
+
    </div>
-    
-    
+
+
 </section>
 
 <!--About me section ends here-->
@@ -291,21 +294,21 @@ Template Name: Home Page
 
 
 <section class="contactSec">
-   
-   <div class="row formControl">
-     
-     <p class="contactP">contact me</p>
-      
-      
-      <?php echo do_shortcode('[contact-form-7 id="90" title="contact"]'); ?>
-       
 
-       
-       
+   <div class="row formControl">
+
+     <p class="contactP">contact me</p>
+
+
+      <?php echo do_shortcode('[contact-form-7 id="90" title="contact"]'); ?>
+
+
+
+
    </div>
-    
-    
-    
+
+
+
 </section>
 
 <!--Contact Me Section Ends Here-->
